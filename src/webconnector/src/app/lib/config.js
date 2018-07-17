c54f1c43d93cd30e93e233cc757c4ac1c57083fb
Ken Yasue <ken.yasue@clover-studio.com>
2018-07-17 14:11:08 +0200

    better url base

--- src/webconnector/src/app/lib/config.js
+++ src/webconnector/src/app/lib/config.js
@@ -1,16 +1,16 @@
 
 // --------------------- must change
 
-export const baseURL = "http://localhost:8080";
+export const baseURL = "/";
 export const SpikaAPIKey = "GtZX9bkKKiWpJKauL06ugOCZS2BwrJEY";
 
-export const SpikaWebSocketURL = 'ws://localhost:8080/spikaenterprise';
+export const SpikaWebSocketURL = '/spikaenterprise';
 
 // -------------------- options
 
-export const SpikaBaseURL = baseURL + "/api/v3";
-export const AvatarBaseURL = baseURL + "/api/v2/avatar/user/";
-export const ImageBaseURL = baseURL + "/api/v2/file/";
+export const SpikaBaseURL = baseURL + "api/v3";
+export const AvatarBaseURL = baseURL + "api/v2/avatar/user/";
+export const ImageBaseURL = baseURL + "api/v2/file/";
 
 export const hashSalt = "8zgqvU6LaziThJI1uz3PevYd";
 export const AESPassword = "cl0v3r-S+uD10-h4X0r1";
